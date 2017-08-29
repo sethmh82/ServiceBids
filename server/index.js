7211bfe80b74aa53dc8af7fd37dc9348d9d4dd2a
Sethmh82 <seth@statisticbrain.com>
2017-08-28 18:49:16 -0700

    update heroic integration

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import Sequelize from 'sequelize';
 import bodyParser from 'body-parser';
 
 var PORT = process.env.PORT || 3000;
-var env       = process.env.NODE_ENV || 'test';
+var env       = process.env.NODE_ENV || 'production';
 
 
 var config = require(path.join(__dirname, 'config', 'config.json'))[env];
