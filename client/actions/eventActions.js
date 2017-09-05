48fba601b2b9bc3124d0d1a69a5c56f0c44fbf93
mehdicode <kalan.mahdi@yahoo.com>
2017-09-05 04:15:12 -0700

    post a project 2

--- client/actions/eventActions.js
+++ client/actions/eventActions.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
-import setAuthorizationToken from '../utils/setAuthorizationToken';
-import jwtDecode from 'jwt-decode';
-import { SET_CURRENT_USER } from './types';
+
 
 export function createEvent(event, id) {
   return dispatch => {
