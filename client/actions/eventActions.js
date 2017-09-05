e1a2d364bf59493188b19aaf8039c601363d44ef
mehdicode <kalan.mahdi@yahoo.com>
2017-09-05 04:12:14 -0700

    post a project 1

--- client/actions/eventActions.js
+++ client/actions/eventActions.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
+import setAuthorizationToken from '../utils/setAuthorizationToken';
+import jwtDecode from 'jwt-decode';
+import { SET_CURRENT_USER } from './types';
 
-export function createEvent(event) {
+export function createEvent(event, id) {
   return dispatch => {
-    return axios.post('/api/events', event);
+    return axios.post(`/api/events/${id}`, event);
   };
 }
+
