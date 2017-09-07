cd33b3acc0ac1d93063e68b64213a70694875478
nicktamburro <nicholastamburro@gmail.com>
2017-09-06 20:42:01 -0700

    one more change, payment stuff

--- client/src/CheckoutApp.js
+++ client/src/CheckoutApp.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import Checkout from './Checkout';
-import './App.css';
 
 class CheckoutApp extends Component {
   render() {
