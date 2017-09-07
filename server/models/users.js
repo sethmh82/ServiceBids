c8662f3b8fc4d4e82f05ab3580129cd4b6ad97b7
Sethmh82 <seth@statisticbrain.com>
2017-09-07 01:19:50 -0700

    sss

--- server/models/users.js
+++ server/models/users.js
@@ -1,60 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
     var users = sequelize.define("users", {
-<<<<<<< HEAD
-  
-      username: {
-          type: DataTypes.STRING(15),
-          allowNull: false,
-          unique: true
-      },
-      password_digest: {
-          type: DataTypes.STRING(60),
-          allowNull: false
-      },
-      email: {
-          type: DataTypes.STRING(100),
-          allowNull: false,
-          unique: true,
-          validate: {
-              isEmail: true
-          }
-      },
-      photo: {
-          type: DataTypes.STRING,
-          allowNull: true
-      },
-      location: {
-          type: DataTypes.STRING,
-          allowNull: false
-      },
-      servicesList: {
-          type: DataTypes.STRING,
-          allowNull: false
-      },
-      about: {
-          type: DataTypes.STRING,
-          allowNull: true
-      },
-      myRating: {
-          type: DataTypes.INTEGER,
-          allowNull: true
-      },
-      myReviews: {
-          type: DataTypes.STRING,
-          allowNull: true
-      },
-    }); 
-  // Class Method
-  users.associate = function (models) {
-  
-     users.hasMany(models.projects, {
-      onDelete: "cascade"
-  });
-      
-  };
-    return users
-  }
-=======
         username: {
             type: DataTypes.STRING(15),
             allowNull: false,
@@ -108,4 +53,3 @@ module.exports = function(sequelize, DataTypes) {
     };
   return users
 }
->>>>>>> origin/master
