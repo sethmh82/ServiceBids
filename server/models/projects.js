83e57b0c84d8eccb50561eae03a2c2d153ecdf6d
kendancy <kendancy@hotmail.com>
2017-09-06 20:58:48 -0700

    put models once more

--- server/models/projects.js
+++ server/models/projects.js
@@ -1,63 +1,62 @@
 module.exports = function(sequelize, DataTypes) {
-  var Projects = sequelize.define("projects", {
-
-    authorID: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    projectName: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },        
-    category: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },        
-    description: {
-        type: DataTypes.STRING,
-        allowNull: true
-    },        
-    location: {
-        type: DataTypes.STRING,
-        allowNull: true
-    },        
-    comments: {
-        type: DataTypes.STRING,
-        allowNull: true
-    },        
-    bids: {
-        type: DataTypes.DECIMAL,
-        allowNull: true
-    },        
-    lowestBidder: {
-        type: DataTypes.STRING,
-        allowNull: true
-    },        
-    timeEnding: {
-        type: DataTypes.TIME,
-        allowNull: true
-    },
-    paymentReceived: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        defaultValue: false
-    },
-    completed: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        defaultValue: false
-    },
-    completedBy: {
-        type: DataTypes.STRING,
-        defaultValue: ""
-    },        
-    rating: {
-        type: DataTypes.INTEGER,
-        allowNull: true
-    }
+    var projects = sequelize.define("projects", {
+        authorID: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        projectName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },        
+        category: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },        
+        description: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },        
+        location: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },        
+        comments: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },        
+        bids: {
+            type: DataTypes.DECIMAL,
+            allowNull: true
+        },        
+        lowestBidder: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },        
+        timeEnding: {
+            type: DataTypes.TIME,
+            allowNull: true
+        },
+        paymentReceived: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        completed: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        completedBy: {
+            type: DataTypes.STRING,
+            defaultValue: ""
+        },        
+        rating: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        }
     }); 
 
-    Projects.associate = function (models) {
+    projects.associate = function (models) {
 
         projects.belongsTo(models.users, {
             foreignKey: {
@@ -66,5 +65,5 @@ module.exports = function(sequelize, DataTypes) {
         });
     
     };
-    return Projects
+    return projects
 };
--- server/models/users.js
+++ server/models/users.js
@@ -1,47 +1,47 @@
 module.exports = function(sequelize, DataTypes) {
-  var Users = sequelize.define("users", {
-    username: {
-        type: DataTypes.STRING(15),
-        allowNull: false,
-        unique: true
-    },
-    password_digest: {
-        type: DataTypes.STRING(60),
-        allowNull: false
-    },
-    email: {
-        type: DataTypes.STRING(100),
-        allowNull: false,
-        unique: true,
-        validate: {
-            isEmail: true
-        }
-    },
-    photo: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    location: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    servicesList: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    about: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    myRating: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    myReviews: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-  }); 
+    var users = sequelize.define("users", {
+        username: {
+            type: DataTypes.STRING(15),
+            allowNull: false,
+            unique: true
+        },
+        password_digest: {
+            type: DataTypes.STRING(60),
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        photo: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        location: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        servicesList: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        about: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        myRating: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        myReviews: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+    }); 
 
   // Class Method
     users.associate = function (models) {
@@ -51,5 +51,5 @@ module.exports = function(sequelize, DataTypes) {
         });
         
     };
-  return Users
+  return users
 }
