1d7cce6055d1ba6aa83877e62f4470a245707db0
Home <Home@dhcp-v067-126.mobile.uci.edu>
2017-09-09 10:01:32 -0700

    fix over serveing

--- server/index.js
+++ server/index.js
@@ -53,7 +53,7 @@ app.use(webpackMiddleware(compiler, {
 app.use(webpackHotMiddleware(compiler));
 
 
-app.use('*', (req, res) => {
+app.use('/', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
 });
 
